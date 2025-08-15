d02c52c65d8d180e005ceeaa8d2d008b41bb1162
Priyanshu Zanzote <piyushzanzote14@gmail.com>
2025-08-15 23:56:08 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class pattern {
+    public static void main(String[] args) {
+        
+        //solid rectangle
+        
+        int n=4;
+        int m=5;
+      for(int i=0;i<4;i++)
+      {
+            for(int j=0;j<5;j++)
+           {
+            System.out.print("*");
+
+            }
+            System.out.println();
+        }
+    }
+}

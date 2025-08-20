4885a73293d9de8eae7432e527b0b260fb444e6a
Priyanshu Zanzote <piyushzanzote14@gmail.com>
2025-08-20 23:25:12 +0530

    Add files via upload

---
+++ pattern4.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class pattern4 {
+    public static void main(String[] args) {
+        {int n=5;
+        int number=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(number+" ");
+                number++;
+            }
+            System.out.println();
+        }}
+        //0 -1 triangle
+
+        int n =5;
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=i;j++){
+                int sum=i+j;
+                if (sum%2==0){
+                    System.out.print("1");
+                }else{
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
